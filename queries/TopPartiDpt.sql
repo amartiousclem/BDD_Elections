c8821ff6670b5fefb9e1cc0ea88732c7e830f194
Mathieu Gicquel <59269440+MathieuGicquel@users.noreply.github.com>
2022-03-16 19:56:51 +0100

    fix

--- queries/TopPartiDpt.sql
+++ queries/TopPartiDpt.sql
@@ -1,4 +1,4 @@
 SELECT nomdpt, parti_politique, sum(nbvote) as votes,
 	DENSE_RANK() OVER (PARTITION BY nomdpt ORDER BY sum(nbvote) desc) rank
-FROM Fait NATURAL JOIN Circonscription
+FROM Fait JOIN CIRCONSCRIPTION C2 on C2.CIRC_EL_ID = FAIT.ID_CIRC
 GROUP BY (nomdpt, parti_politique);
