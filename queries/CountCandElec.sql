9ace0af2f70a5b8169105a6f697a51ce21184372
Mathieu Gicquel <59269440+MathieuGicquel@users.noreply.github.com>
2022-03-21 22:46:15 +0100

    fix

--- queries/CountCandElec.sql
+++ queries/CountCandElec.sql
@@ -1,4 +1,7 @@
-SELECT id_candidat, COUNT(DISTINCT id_election) as nb
-FROM (Fait NATURAL JOIN Election)
+SELECT id_candidat, COUNT(DISTINCT CONCAT(ID_ELECTION, id_candidat)) as nb
+FROM (Fait JOIN Election ON FAIT.ID_ELECTION = ELECTION.ELECTION_ID)
+WHERE election_tour = 1
+GROUP BY ROLLUP(id_candidat);SELECT id_candidat, COUNT(DISTINCT CONCAT(ID_ELECTION, id_candidat)) as nb
+FROM (Fait JOIN Election ON FAIT.ID_ELECTION = ELECTION.ELECTION_ID)
 WHERE election_tour = 1
 GROUP BY ROLLUP(id_candidat);
